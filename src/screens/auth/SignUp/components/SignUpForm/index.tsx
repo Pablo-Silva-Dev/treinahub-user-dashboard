6c181db6aa09fc22c8450c84d123e68815988a6a
Pablo <pablolucio_@hotmail.com>
2024-09-19 07:38:31 -0300

    fix: fixes password confirmation validation at registering a new user

--- src/screens/auth/SignUp/components/SignUpForm/index.tsx
+++ src/screens/auth/SignUp/components/SignUpForm/index.tsx
@@ -252,6 +252,7 @@ export default function SignUpForm({
             disabled={
               !isValid ||
               !passwordValidated.current ||
+              passwordValue !== passwordConfirmation ||
               !wasTermsAccepted ||
               isLoading
             }
